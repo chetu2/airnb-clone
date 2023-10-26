9f1ba0f4776f7b84d4463fcca61f521b6541ed72
chetu2 <113667749+chetu2@users.noreply.github.com>
2023-10-26 09:31:42 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,68 @@
+export default [
+    {
+        id: 1,
+        title: "Villa",
+        description: "Top-end Space",
+        price: 136,
+        coverImg: "im1.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "London",
+        openSpots: 0,
+    },
+
+    {
+        id: 2,
+        title: "Villa",
+        description: "Crazy Town",
+        price: 125,
+        coverImg: "im2.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Dubai",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Bungalows",
+        description: "Crazy Peak",
+        price: 60,
+        coverImg: "im3.png",
+        stats: {
+            rating: 4.3,
+            reviewCount: 6
+        },
+        location: "Spain",
+        openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Villas",
+        description: "Majestic Villa",
+        price: 70,
+        coverImg: "im4.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "France",
+        openSpots: 3,
+    },
+    {
+        id: 5,
+        title: "Bungalows",
+        description: "HighEnd Villa",
+        price: 50,
+        coverImg: "im6.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "France",
+        openSpots: 3,
+    }
+]
